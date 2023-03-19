3bf1ed4805679f6ed89e9dc28bf1c5b5ec788ff1
Jon Ng <jonathanng15@gmail.com>
2023-03-19 01:59:18 -0700

    Display dummy messages on screen and added stylign

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+const port = 3000;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,12 +24,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -36,6 +37,10 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    title: 'Hi there!',
+    text: 'lorem sfaslorem sfaslorem sfaslorem sfaslorem sfaslorem sfaslorem sfaslorem sfaslorem sfaslorem sfaslorem sfaslorem sfaslorem sfaslorem sfaslorem sfaslorem sfaslorem sfaslorem sfaslorem sfaslorem sfaslorem sfaslorem sfaslorem sfaslorem sfaslorem sfaslorem sfaslorem sfaslorem sfaslorem sfaslorem',
+    user: 'Amando',
+    added: new Date(),
+  },
+  {
+    title: 'Hello World!',
+    text: 'lorem sfasdf',
+    user: 'Charles',
+    added: new Date(),
+  },
+];
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', { messages: messages });
 });
 
 module.exports = router;
